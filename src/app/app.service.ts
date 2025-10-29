68fc9842586a1d39e646c976a7eb640816574327
Carlos Cabrera <carloscabre_96@hotmail.com>
2025-10-29 01:06:15 +0100

    Cambio de version

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -37,7 +37,7 @@ export class AppService {
   public parametrosProyecto: any = {};
 
   //Variables Globales:
-  public version: string = "0.9.95";
+  public version: string = "0.9.96";
 
   cambiarUrl(url: string): void {
     console.log("CAMBIANDO A URL: " + url);
