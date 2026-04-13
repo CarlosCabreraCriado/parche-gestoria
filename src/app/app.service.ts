bbcc6961e8eb39093c6e776fb5d4bfeef8d689be
Gonzalo <tagorus.gonzalo@gmail.com>
2026-04-13 23:02:28 +0100

    0.92.1

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -37,7 +37,7 @@ export class AppService {
   public parametrosProyecto: any = {};
 
   //Variables Globales:
-  public version: string = "0.92.0";
+  public version: string = "0.92.1";
 
   cambiarUrl(url: string): void {
     console.log("CAMBIANDO A URL: " + url);
