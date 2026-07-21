fcab8dacf96f0d0964419657b02ecec14381a5c1
Gonzalo <tagorus.gonzalo@gmail.com>
2026-07-21 12:44:24 +0100

    chore(version): actualizar version de 0.93.7 a 0.93.8

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -37,7 +37,7 @@ export class AppService {
   public parametrosProyecto: any = {};
 
   //Variables Globales:
-  public version: string = "0.93.7";
+  public version: string = "0.93.8";
 
   cambiarUrl(url: string): void {
     console.log("CAMBIANDO A URL: " + url);
